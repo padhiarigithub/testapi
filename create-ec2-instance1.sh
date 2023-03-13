3a64071aa8216b9600dbca8bf8b14337f54700c1
padhiarigithub <111277991+padhiarigithub@users.noreply.github.com>
2023-03-13 10:37:11 +0530

    Update create-ec2-instance1.sh

--- create-ec2-instance1.sh
+++ create-ec2-instance1.sh
@@ -1,44 +1,43 @@
 #!/bin/bash
 
-# Set your AWS region
-export AWS_DEFAULT_REGION=ap-southeast-1
+# Set variables for EC2 instance configuration
+AMI_ID="ami-082b1f4237bd816a1" # replace with desired AMI ID
+INSTANCE_TYPE="t2.micro"
+SECURITY_GROUP_ID="sg-017f5ac79caac7c6b" # replace with desired security group ID
+SUBNET_ID="subnet-0bb79b484d5a54899" # replace with desired subnet ID
+KEY_NAME="freqtrade" # replace with desired key pair name
+REGION="ap-southeast-1"
+SCRIPT_URL="https://raw.githubusercontent.com/padhiarigithub/testapi/main/run.sh"
 
-# Set your key pair name
-KEY_NAME=freqtrade
+# Create instance
+INSTANCE_ID=$(aws ec2 run-instances \
+  --image-id $AMI_ID \
+  --instance-type $INSTANCE_TYPE \
+  --key-name $KEY_NAME \
+  --region $REGION \
+  --query 'Instances[0].InstanceId' \
+  --output text)
 
-# Set the security group id
-SECURITY_GROUP_NAME=launch-wizard-2
-
-# Set the AMI ID
-AMI_ID=ami-082b1f4237bd816a1
-
-# Set the instance type
-INSTANCE_TYPE=t2.micro
-
-# Set the script file path
-SCRIPT_FILE_PATH=https://github.com/padhiarigithub/testapi/blob/main/run.sh
-
-# Create the security group
-aws ec2 create-security-group --group-name $SECURITY_GROUP_NAME --description "My security group"
-
-# Open port 22 for SSH
-aws ec2 authorize-security-group-ingress --group-name $SECURITY_GROUP_NAME --protocol tcp --port 22 --cidr 0.0.0.0/0
-
-# Launch the instance
-INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type $INSTANCE_TYPE --key-name $KEY_NAME --security-groups $SECURITY_GROUP_NAME --query 'Instances[0].InstanceId' --output text)
-
-echo "Instance created with ID: $INSTANCE_ID"
-
-# Wait for the instance to start running
+# Wait for instance to be running
 aws ec2 wait instance-running --instance-ids $INSTANCE_ID
 
-# Get the public IP address of the instance
-PUBLIC_IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+# Get instance public IP address
+PUBLIC_IP=$(aws ec2 describe-instances \
+  --instance-ids $INSTANCE_ID \
+  --region $REGION \
+  --query 'Reservations[0].Instances[0].PublicIpAddress' \
+  --output text)
 
-echo "Public IP address of the instance: $PUBLIC_IP"
+# Download script to instance
+ssh -i  ~/Downloads/freqtrade.pem ubuntu@$PUBLIC_IP wget -O run.sh $SCRIPT_URL
 
-# Copy the script file to the instance
-scp -i https://github.com/padhiarigithub/testapi/blob/main/$KEY_NAME.pem $SCRIPT_FILE_PATH ubuntu@$PUBLIC_IP:~
 
-# Run the script on the instance
-ssh -i https://github.com/padhiarigithub/testapi/blob/main/$KEY_NAME.pem ubuntu@$PUBLIC_IP 'bash ~/run.sh'
+# Run script on instance
+ssh -i  ~/Downloads/freqtrade.pem ubuntu@$PUBLIC_IP 'bash run.sh'
+
+
+
+# Terminate instance after 5 minutes
+sleep 300
+aws ec2 terminate-instances --instance-ids $INSTANCE_ID
+
