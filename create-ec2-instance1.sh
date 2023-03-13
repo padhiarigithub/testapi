19d0abadc9efe6e1babc3259074a1f446514fc9d
padhiarigithub <111277991+padhiarigithub@users.noreply.github.com>
2023-03-13 16:21:13 +0530

    Update create-ec2-instance1.sh

--- create-ec2-instance1.sh
+++ create-ec2-instance1.sh
@@ -5,7 +5,7 @@ AMI_ID="ami-082b1f4237bd816a1" # replace with desired AMI ID
 INSTANCE_TYPE="t2.micro"
 SECURITY_GROUP_ID="sg-017f5ac79caac7c6b" # replace with desired security group ID
 SUBNET_ID="subnet-0bb79b484d5a54899" # replace with desired subnet ID
-KEY_NAME="freqtrade.pem" # replace with desired key pair name
+KEY_NAME="freqtrade" # replace with desired key pair name
 REGION="ap-southeast-1"
 SCRIPT_URL="https://raw.githubusercontent.com/padhiarigithub/testapi/main/run.sh"
 
@@ -29,16 +29,15 @@ PUBLIC_IP=$(aws ec2 describe-instances \
   --output text)
 
 # Download script to instance
-scp -i  "C:\Users\pc\Downloads\freqtrade.pem" "C:\Users\pc\Downloads\run.sh" ubuntu@$PUBLIC_IP:/home/ec2-user/
+scp -i  "C:\Users\pc\Downloads\my_keypair.pem" "C:\Users\pc\Downloads\run.sh" ubuntu@$PUBLIC_IP:/home/ec2-user/
 
 
 # Run script on instance
-cd /home/ec2-user/
-ssh -i  "freqtrade.pem" ubuntu@$PUBLIC_IP 'bash /home/ec2-user/run.sh'
+ssh -i  "C:\Users\pc\Downloads\my_keypair.pem" ubuntu@$PUBLIC_IP 'bash /home/ec2-user/run.sh'
 
 
 
-# Terminate instance after 2 minutes
-sleep 600
+# Terminate instance after 5 minutes
+sleep 300
 aws ec2 terminate-instances --instance-ids $INSTANCE_ID
 
